279da73b02a3d8a1aaae46c6bea1461df9c4b3d9
Rich Data Space <richdataspace@gmail.com>
2021-01-28 00:02:41 -0300

    Nova organizacao no repo

--- frontend/public/script.js
+++
@@ -1,5 +0,0 @@
-// Babel: Converte um código em outro - Converte um codigo do react para que seja possivel o browser interpretar;
-// WebPack: Para cada tipo de arquivo o codigo eh convetido de maneira diferente (.js, .css, .png);
-
-// Loeaders: babel - loader,  css - loader, image - loader, file - loader. (Utilizados pelo webpack);
-
--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
-
-
-// JSX: HTML DENTRO DO JS
-
-render(<App />, document.getElementById('app'));
